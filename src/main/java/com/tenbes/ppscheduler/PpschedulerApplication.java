40cc5cffc1df3a51a826b4565393748d158717f2
laz1r <ecabanban1@gmail.com>
2021-05-23 15:20:12 -0700

    Initial commit

---
+++ src/main/java/com/tenbes/ppscheduler/PpschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tenbes.ppscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenbes/ppscheduler/PpschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenbes.ppscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
